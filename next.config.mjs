75de66e9ece00f0df71fa33fefa4f88a2ec570fc
mariam-elsarag <mariamelsarag44@gmail.com>
2026-04-05 15:38:33 +0200

    fix build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    serverExternalPackages: ["@prisma/client"],
+  },
   images: {
     remotePatterns: [
       {
@@ -13,7 +16,6 @@ const nextConfig = {
       },
     ],
   },
-  // output: "export",
 };
 
 export default nextConfig;
